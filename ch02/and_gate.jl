9681f8efae71c0ba521db17c39754f9f0ce6b74b
szmlb <szmlb.robotics@gmail.com>
2018-09-23 23:50:05 +0900

    Add ch02

---
+++ ch02/and_gate.jl
@@ -0,0 +1,18 @@
+function AND(x1, x2)
+    x = [x1, x2]
+    w = [0.5, 0.5]
+    b = -0.7
+    tmp = w'*x + b
+    if tmp <= 0
+        return 0
+    else
+        return 1
+    end
+end
+
+if occursin(PROGRAM_FILE, @__FILE__)
+    for xs in [[0, 0], [1, 0], [0, 1], [1, 1]]
+        y = AND(xs[1], xs[2])
+        println(xs, " -> ", y)
+    end
+end
---
+++ ch02/xor_gate.jl
@@ -0,0 +1,17 @@
+include("and_gate.jl")
+include("or_gate.jl")
+include("nand_gate.jl")
+
+function XOR(x1, x2)
+    s1 = NAND(x1, x2)
+    s2 = OR(x1,  x2)
+    y = AND(s1, s2)
+    return y
+end
+
+if occursin(PROGRAM_FILE, @__FILE__)
+    for xs in [[0, 0], [1, 0], [0, 1], [1, 1]]
+        y = AND(xs[1], xs[2])
+        println(xs, " -> ", y)
+    end
+end
