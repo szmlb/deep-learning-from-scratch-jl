66b52fa4871afab0285f3dc89a69fe8c7bbf95de
szmlb <szmlb.robotics@gmail.com>
2018-10-31 23:37:39 +0900

    Modified gradient_1d.jl

--- ch04/gradient_1d.jl
+++ ch04/gradient_1d.jl
@@ -2,29 +2,37 @@ import PyPlot
 
 function numerical_diff(f, x)
     h = 1e-4 # 0.0001
-    return (f(x+h) .- f(x-h)) ./ (2*h)
+    return (f(x+h) - f(x-h)) / (2*h)
 end
 
 function function_1(x)
-    return 0.01 .* x.^2 .+ 0.1 .* x
+    return 0.01x.^2 + 0.1x
 end
 
 function tangent_line(f, x)
     d = numerical_diff(f, x)
     println(d)
-    y = f(x) .- d .* x
-    return (t) -> d .* t .+ y
+    y = f(x) - d * x
+    return (t) -> d * t + y
 end
 
-x = collect(0.0:0.1:20.0)
-y = function_1(x)
+function main()
 
-PyPlot.xlabel("x")
-PyPlot.ylabel("f(x)")
+  x = collect(0.0:0.1:20.0)
+  y = function_1(x)
 
-tf = tangent_line(function_1, 5)
-y2 = tf(x)
+  PyPlot.xlabel("x")
+  PyPlot.ylabel("f(x)")
 
-PyPlot.plot(x, y)
-PyPlot.plot(x, y2)
-PyPlot.show()
+  tf = tangent_line(function_1, 5)
+  y2 = tf(x)
+
+  PyPlot.plot(x, y)
+  PyPlot.plot(x, y2)
+  PyPlot.show()
+
+end
+
+if occursin(PROGRAM_FILE, @__FILE__)
+    main()
+end
