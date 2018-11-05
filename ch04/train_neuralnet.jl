20a0fb36fdfcf4d86650f6b55af5841b278b9b1b
szmlb <szmlb.robotics@gmail.com>
2018-11-06 02:00:16 +0900

    Modified train_neuralnet.jl and two_layer_net.jl

--- ch04/train_neuralnet.jl
+++ ch04/train_neuralnet.jl
@@ -3,10 +3,29 @@ using Random
 include("two_layer_net.jl")
 import MLDatasets: MNIST
 
+function num2onehot(num_array, max_num)
+
+  onehot_array = zeros(Int, (max_num, length(num_array)))
+
+  for i in 1:length(num_array)
+    for j in 1:max_num
+      if j == num_array[i]
+        onehot_array[j, i] = 1
+      else
+        onehot_array[j, i] = 0
+      end
+    end
+  end
+
+  return onehot_array
+
+end
+
 function main()
 
     x_train = MNIST.convert2features(MNIST.traintensor())
     t_train = MNIST.trainlabels()
+    t_train = num2onehot(t_train, 10)
 
     input_size = 784
     hidden_size = 50
@@ -15,7 +34,7 @@ function main()
     network = TwoLayerNet(input_size, hidden_size, output_size, weight_init_std)
 
     iters_num = 10000  # 繰り返しの回数を適宜設定する
-    train_size = size(x_train)[1]
+    train_size = size(x_train)[2]
     batch_size = 100
     learning_rate = 0.1
 
@@ -24,6 +43,24 @@ function main()
     test_acc_list = []
 
     iter_per_epoch = max(train_size / batch_size, 1)
+
+    rng = MersenneTwister(1234)
+    for i in 1:iters_num
+
+        batch_mask = rand(1:train_size,  batch_size)
+        x_batch = x_train[:, batch_mask]
+        t_batch = t_train[:, batch_mask]
+
+        # 勾配の計算
+        grad = numerical_gradient(network, x_batch, t_batch)
+        #grad = network.gradient(x_batch, t_batch)
+
+        # パラメータの更新
+        for key in ["W1", "b1", "W2", "b2"]
+            network.params[key] -= learning_rate * grad[key]
+        end
+
+    end
 
 end
 
--- ch04/two_layer_net.jl
+++ ch04/two_layer_net.jl
@@ -13,14 +13,102 @@ mutable struct TwoLayerNet
 
         params = Dict()
         params["W1"] = weight_init_std * randn(rng, Float64,  (hidden_size,  input_size))
-        params["b1"] = Array{Float64}(undef, hidden_size)
+        params["b1"] = zeros(Float64, hidden_size)
         params["W2"] = weight_init_std * randn(rng, Float64,  (output_size,  hidden_size))
-        params["b2"] = Array{Float64}(undef, output_size)
+        params["b2"] = zeros(Float64, output_size)
 
         new(params)
+
     end
 
 end
+
+function predict(self::TwoLayerNet, x)
+
+    W1, W2 = self.params["W1"], self.params["W2"]
+    b1, b2 = self.params["b1"], self.params["b2"]
+
+    a1 = W1 * x + b1
+    z1 = sigmoid(a1)
+    a2 = W2 * z1 + b2
+    y = softmax(a2)
+
+    return y
+
+end
+
+function loss(self::TwoLayerNet, x, t)
+
+    y = predict(self, x)
+
+    return cross_entropy_error(y, t)
+
+end
+
+function accuracy(self::TwoLayerNet)
+
+    y = predict(self, x)
+
+    y = mapslices(argmax, y, dims=1)
+    y = y'
+    t = mapslices(argmax, t, dims=1)
+    t = t'
+
+    accuracy_cnt = 0
+    for i in 1:length(x[:, 1])
+        if y[i] == t[i]
+            accuracy_cnt += 1
+        end
+    end
+
+    return Float64(accuracy_cnt) / length(x[:, 1])
+
+end
+
+# x:入力データ, t:教師データ
+function numerical_gradient(self::TwoLayerNet, x, t)
+
+    loss_W = (W) -> loss(self, x, t) # (f関数名) = (引数名) -> (返り値)
+
+    grads = Dict()
+    grads["W1"] = numerical_gradient_(loss_W, self.params["W1"])
+    grads["b1"] = numerical_gradient_(loss_W, self.params["b1"])
+    grads["W2"] = numerical_gradient_(loss_W, self.params["W2"])
+    grads["b2"] = numerical_gradient_(loss_W, self.params["b2"])
+
+    return grads
+
+end
+
+#=
+function gradient(self::TwoLayerNet, x, t)
+
+    W1, W2 = self.params["W1"], self.params["W2"]
+    b1, b2 = self.params["b1"], self.params["b2"]
+    grads = Dict()
+
+    batch_num = length(x[1, :])
+
+    # forward
+    a1 = W1 * x + b1
+    z1 = sigmoid(a1)
+    a2 = W2 * z1 + b2
+    y = softmax(a2)
+
+    # backward
+    dy = (y - t) / batch_num
+    grads["W2"] = z1' * dy
+    grads["b2"] = sum(dy, dims=1)
+
+    da1 = W2 * dy
+    dz1 = sigmoid_grad(a1) * da1
+    grads["W1"] = x' * dz1
+    grads["b1"] = sum(dz1, dims=1)
+
+    return grads
+
+end
+=#
 
 function main()
 
@@ -29,7 +117,6 @@ function main()
   output_size = 3
   weight_init_std = 0.01
   net = TwoLayerNet(input_size,  hidden_size,  output_size,  weight_init_std)
-  print(net.params["W1"])
 
 end
 
